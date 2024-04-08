c0c0197b43086110293c9eb3db95dc68858027c5
myztinen <myztinen@gmail.com>
2024-04-08 18:32:53 +0300

    Added comment

--- src/untestable1.mjs
+++ src/untestable1.mjs
@@ -1,5 +1,6 @@
 const millisPerDay = 24 * 60 * 60 * 1000;
-
+// Code did not have any parameters. Results were depending on the date that code was ran. 
+// Having date as a parameter makes the code testable.
 export function daysUntilChristmas(year, month, day) {
   const now = new Date(year, month-1, day);
   const today = new Date(now.getFullYear(), now.getMonth(), now.getDate());
