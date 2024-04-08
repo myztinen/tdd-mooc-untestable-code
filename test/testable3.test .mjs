1d6e41623d8382307353534ed9c356fc324103e1
myztinen <myztinen@gmail.com>
2024-04-08 20:51:33 +0300

    Third part done

--- test/testable3.test .mjs
+++ test/testable3.test .mjs
@@ -1,12 +1,14 @@
 import { describe, test } from "vitest";
 import { expect } from "chai";
 import { parsePeopleCsv, readFileData } from "../src/testable3.mjs";
-dfdg
+
 // example input:
 // Loid,Forger,,Male
 // Anya,Forger,6,Female
 // Yor,Forger,27,Female
 
+// For some reason I was not able to get Vitest to run testable3.test.mjs so I added tests to 
+// untestable3.test.mjs 
 describe("Untestable 3: CSV file parsing", () => {
   test("File content can be read", async () => {
     expect(await readFileData("./test.csv")).to.deep.equal();
--- test/untestable3.test.mjs
+++ test/untestable3.test.mjs
@@ -1,17 +1,56 @@
 import { describe, test } from "vitest";
 import { expect } from "chai";
-import { parsePeopleCsv } from "../src/untestable3.mjs";
+import { parsePeopleCsv, readFileData } from "../src/testable3.mjs";
 
-// example input:
-// Loid,Forger,,Male
-// Anya,Forger,6,Female
-// Yor,Forger,27,Female
+
+// For some reason I was not able to get Vitest to run testable2.test.mjs so I added tests to 
+// this file
+const input = `
+      Loid,Forger,,Male
+      Anya,Forger,6,Female
+      Yor,Forger,27,Female`;
 
 describe("Untestable 3: CSV file parsing", () => {
-  test("todo", async () => {
-    // TODO: write proper tests
-    try {
-      expect(await parsePeopleCsv("people.csv")).to.deep.equal([]);
-    } catch (e) {}
+  test("File content can be read", async () => {
+    expect(await readFileData("./test/test.csv")).to.equal("hello world!");
+  });
+
+  test("File can be parsed", async () => {
+    expect(parsePeopleCsv(input)).to.deep.equal([
+      { firstName: "Loid", lastName: "Forger", gender: "m" },
+      { firstName: "Anya", lastName: "Forger", age: 6, gender: "f" },
+      { firstName: "Yor", lastName: "Forger", age: 27, gender: "f" }]);
+  });
+
+  const input2 = `
+         Yor,Forger,27,Female`;
+
+  test("Sinlle lines can be parsed", async () => {
+    let result = parsePeopleCsv(input2);
+    expect(result).to.deep.equal([
+      { firstName: "Yor", lastName: "Forger", age: 27, gender: "f" }]);
+    expect(result[0].firstName).to.equal("Yor");
+    expect(result[0].lastName).to.equal("Forger");
+    expect(result[0].age).to.equal(27);
+    expect(result[0].gender).to.equal("f");
+
+  });
+
+  const input3 = `
+  Loid,Forger,,Male
+  
+  Yor,Forger,27,Female`;
+
+  test("Empty lines are ignored", async () => {
+    expect(parsePeopleCsv(input3)).to.deep.equal([
+      { firstName: "Loid", lastName: "Forger", gender: "m" },
+      { firstName: "Yor", lastName: "Forger", age: 27, gender: "f" }]);
+  });
+
+  const input4 = `
+  Loid,Forger,,Male`;
+
+  test("Age is not mandatory", async () => {
+    expect(parsePeopleCsv(input4)[0].age).to.equal(undefined);
   });
 });
