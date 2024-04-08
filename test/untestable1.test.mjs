6648c204af5c039a4250ebea82cc93aa8cc90aaf
myztinen <myztinen@gmail.com>
2024-04-08 18:44:07 +0300

    Read instructions better and fixed first solution

--- test/untestable1.test.mjs
+++ test/untestable1.test.mjs
@@ -3,23 +3,8 @@ import { expect } from "chai";
 import { daysUntilChristmas } from "../src/untestable1.mjs";
 
 describe("Untestable 1: days until Christmas", () => {
-  test("first of December", () => {
-    expect(daysUntilChristmas(2024, 12, 1)).to.equal(24);
-  });
-
-  test("Christmas day", () => {
-    expect(daysUntilChristmas(2024, 12, 25)).to.equal(0);
-  });
-
-  test("Christmas eve", () => {
-    expect(daysUntilChristmas(2024, 12, 24)).to.equal(1);
-  });
-
-  test("Boxing day", () => {
-    expect(daysUntilChristmas(2024, 12, 26)).to.equal(364);
-  });
-
-  test("Midsummer", () => {
-    expect(daysUntilChristmas(2024, 6, 21)).to.equal(187);
+  test("todo", () => {
+    // TODO: write proper tests
+    expect(daysUntilChristmas()).to.be.a("number");
   });
 });
